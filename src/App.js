c346cd90552203ec4f8daa4213fbc7818cd57e5b
hamza_khaled <khaledhamza251785@gmail.com>
2023-10-17 15:40:51 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navv from "./Navv"
+import Cardd from "./Cardd"
+
+function App() {
+  return (
+    <div className="App">
+  
+        <h1>Welcome To React </h1>
+        <Navv/>
+        <Cardd/>
+        <Cardd/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cardd.jsx
@@ -0,0 +1,22 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function Cardd() {
+  return (
+    <div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://thumbs.dreamstime.com/z/jour-de-terre-d-environnement-dans-les-mains-des-arbres-cultivant-jeunes-plantes-bokeh-verdissent-la-main-femelle-fond-tenant-l-130247647.jpg?w=992" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </div>
+  );
+}
+
+export default Cardd;
